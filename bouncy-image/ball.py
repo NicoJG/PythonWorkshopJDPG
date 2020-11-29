ead5c106ed5cd9b9a52deacefae5b018f1987f25
Nico Guth <nico.guth@tu-dortmund.de>
2020-11-29 23:43:16 +0100

    draw the balls as patches.Circle

--- bouncy-image/ball.py
+++ bouncy-image/ball.py
@@ -3,11 +3,13 @@ import matplotlib
 
 # class for the bouncing balls
 class Ball:
-    r = 0.01
+    r = 0.02
     def __init__(self, x, y, color):
         self.x = x
         self.y = y
         self.color = color
+        # create matplotlib representation
+        self.artist = matplotlib.patches.Circle(xy=(self.x,self.y), radius=self.r, color=self.color)
         # starting velocity
         self.v_x = np.random.uniform(0.5,2)*np.random.choice([-1,1])
         self.v_y = np.random.uniform(0.5,2)*np.random.choice([-1,1])
@@ -16,6 +18,8 @@ class Ball:
         self.x += self.v_x*dt
         self.y += self.v_y*dt
         self.boundary_collision(boundary)
+        self.artist.center = (self.x,self.y)
+        self.artist.radius = self.r
 
     def boundary_collision(self, boundary):
         if self.x-self.r <= boundary.l:
--- bouncy-image/bouncy_image.py
+++ bouncy-image/bouncy_image.py
@@ -10,26 +10,29 @@ from boundary import Boundary
 # general constants
 animation_time = 2 # how many seconds should the animation run
 fps = 60 # frames per second
-n = 5 # number of balls
+n = 100 # number of balls
 boundary = Boundary(-1,1,-1,1) # ball container
 
 # init plot
 fig,ax = plt.subplots()
-ln = plt.plot([],[],"o")[0]
+ln = plt.plot([],[])[0]
 
 # init_func called before the first frame
 def init_animation():
-    global ln,balls
+    global balls
     # init balls
     balls = []
+    ax.patches = []
     for i in range(n):
-        balls.append(Ball(0,0,"blue"))
+        ball = Ball(0,0,"blue")
+        balls.append(ball)
+        ax.add_patch(ball.artist)
 
     # init limits
     ax.set_xlim(boundary.l, boundary.r)
     ax.set_ylim(boundary.b, boundary.t)
     
-    return ln,
+    return ax.patches
 
 # generator function for each frames delta time
 def calc_dt():
@@ -38,20 +41,16 @@ def calc_dt():
     while last_time-starting_time < animation_time:
         dt = time.time()-last_time # in seconds
         last_time = time.time()
+        print(dt)
         yield dt
 
 # does the animation
 def update_animation(dt):
-    global ln,balls
-    x_data = []
-    y_data = []
+    global balls
     for ball in balls:
         ball.update(dt, boundary)
-        x_data.append(ball.x)
-        y_data.append(ball.y)
-    
-    ln.set_data(x_data, y_data)
-    return ln,
+
+    return ax.patches
 
 ani = anim.FuncAnimation(fig, update_animation, frames=calc_dt, init_func=init_animation, interval=1000/fps, blit=True)
 
