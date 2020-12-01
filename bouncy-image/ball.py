e696d0a73c8067f653d8d7d25e67f1282db6c3bd
Nico Guth <nico.guth@tu-dortmund.de>
2020-12-02 00:06:57 +0100

    Physically Accurate Collisions

--- bouncy-image/ball.py
+++ bouncy-image/ball.py
@@ -3,34 +3,51 @@ import matplotlib
 
 # class for the bouncing balls
 class Ball:
-    r = 0.02
+    r = 0.01
     def __init__(self, x, y, color):
-        self.pos = np.array([x,y])
+        # position vector x
+        self.x = np.array([x,y])
         self.color = color
         # create matplotlib representation
-        self.artist = matplotlib.patches.Circle(xy=(self.pos[0],self.pos[1]), radius=self.r, color=self.color)
+        self.artist = matplotlib.patches.Circle(xy=(self.x[0],self.x[1]), radius=self.r, color=self.color)
         # starting velocity
         self.v = np.array([np.random.uniform(0.5,2),0])
         rotate = lambda phi : np.array([ [np.cos(phi), -np.sin(phi)], [np.sin(phi),  np.cos(phi)] ])
         self.v = rotate(np.random.uniform(0,2*np.pi)).dot(self.v)
         
+    def move(self, dt):
+        self.x += self.v*dt
 
-    def update(self, dt, boundary):
-        self.pos += self.v*dt
-        self.boundary_collision(boundary)
-        self.artist.center = (self.pos[0],self.pos[1])
+    def update_artist(self):
+        self.artist.center = (self.x[0],self.x[1])
         self.artist.radius = self.r
 
     def boundary_collision(self, boundary):
-        if self.pos[0]-self.r <= boundary.l:
-            self.pos[0] = boundary.l + self.r
+        if self.x[0]-self.r <= boundary.l:
+            self.x[0] = boundary.l + self.r
             self.v[0] *= -1
-        elif self.pos[0]+self.r >= boundary.r:
-            self.pos[0] = boundary.r - self.r
+        elif self.x[0]+self.r >= boundary.r:
+            self.x[0] = boundary.r - self.r
             self.v[0] *= -1
-        if self.pos[1]-self.r <= boundary.b:
-            self.pos[1] = boundary.b + self.r
+        if self.x[1]-self.r <= boundary.b:
+            self.x[1] = boundary.b + self.r
             self.v[1] *= -1
-        elif self.pos[1]+self.r >= boundary.t:
-            self.pos[1] = boundary.t - self.r
+        elif self.x[1]+self.r >= boundary.t:
+            self.x[1] = boundary.t - self.r
             self.v[1] *= -1
+
+    @staticmethod
+    def ball_collision(a, b):
+        if np.linalg.norm(a.x - b.x) <= 2*a.r :
+            # do collision (assume same masses and equal radii)
+            # set the positions so that they are barely touching and not overlapping
+            d = a.x - b.x
+            n = (1/np.linalg.norm(d))*d
+            a.x = a.x - 1/2*d + a.r*n
+            b.x = b.x + 1/2*d - a.r*n
+            # calc the velocities
+            # Wikipedia "Elastic collision"
+            # v_1' = v_1 - 2*m_2/(m_1+m_2)*<v_1-v_2,x_1-x_2>/||x_1-x_2||^2 * (x_1-x_2)
+            a.v = a.v - np.dot(a.v-b.v,a.x-b.x)/np.linalg.norm(a.x-b.x)**2 * (a.x-b.x)
+            b.v = b.v - np.dot(b.v-a.v,b.x-a.x)/np.linalg.norm(b.x-a.x)**2 * (b.x-a.x)
+
--- bouncy-image/bouncy_image.py
+++ bouncy-image/bouncy_image.py
@@ -9,7 +9,7 @@ from ball import Ball
 from boundary import Boundary
 
 # general constants
-animation_time = 2 # how many seconds should the animation run
+animation_time = 10 # how many seconds should the animation run
 fps = 60 # frames per second
 n = 100 # number of balls
 boundary = Boundary(-1,1,-1,1) # ball container
@@ -25,14 +25,15 @@ def init_animation():
     balls = []
     ax.patches = []
     for i in range(n):
-        ball = Ball(0.,0.,(np.random.rand(),np.random.rand(),np.random.rand()))
+        x = np.random.uniform(boundary.l,boundary.r)
+        y = np.random.uniform(boundary.b,boundary.t)
+        ball = Ball(x,y,(np.random.rand(),np.random.rand(),np.random.rand()))
         balls.append(ball)
         ax.add_patch(ball.artist)
 
     # init limits
     ax.set_xlim(boundary.l, boundary.r)
     ax.set_ylim(boundary.b, boundary.t)
-    
     return ax.patches
 
 # generator function for each frames delta time
@@ -47,8 +48,21 @@ def calc_dt():
 # does the animation
 def update_animation(dt):
     global balls
+
+    # move every ball and check the boundary collision
     for ball in balls:
-        ball.update(dt, boundary)
+        ball.move(dt)
+        ball.boundary_collision(boundary)
+    
+    # check every ball pair for a collision
+    for i in range(len(balls)-1):
+        for j in range(i+1,len(balls)):
+            # TODO: check if balls are in the same part of space rather than check every pair
+            Ball.ball_collision(balls[i],balls[j])
+    
+    # update the visuals
+    for ball in balls:
+        ball.update_artist()
 
     return ax.patches
 
