5461b64a3307b08a0e2a4d6675540e9feddf7a65
Nico Guth <nico.guth@tu-dortmund.de>
2020-12-05 18:58:17 +0100

    A new Repository for the bouncy-image project, because it got bigger than I thought

--- bouncy-image/boundary.py
+++
@@ -1,9 +0,0 @@
-# class for the boundaries in which the balls should be contained
-class Boundary:
-    def __init__(self, left, right, bottom, top):
-        self.l = left
-        self.r = right 
-        self.t = top 
-        self.b = bottom
-        self.width = abs(self.r-self.l)
-        self.height = abs(self.t-self.b)
